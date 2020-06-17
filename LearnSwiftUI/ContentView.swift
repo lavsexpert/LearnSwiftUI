97488ecf53c85b44a392de31725e4a3c4a67b508
Лавров Сергей <lavrov-sergey@yandex.ru>
2020-06-18 01:50:24 +0300

    Initial Commit

---
+++ LearnSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearnSwiftUI
+//
+//  Created by Sergey Lavrov on 18.06.2020.
+//  Copyright © 2020 +1. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
